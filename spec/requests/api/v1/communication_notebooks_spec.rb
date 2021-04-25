a4841c6383dfb862b00a95dd930f10a47c2576d2
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-26 00:18:15 +0900

    [update]editアクションで4月25日の連絡帳を取得するテストを書くため日付を固定に変更

--- spec/requests/api/v1/communication_notebooks_spec.rb
+++ spec/requests/api/v1/communication_notebooks_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe "Api::V1::CommunicationNotebooks", type: :request do
       expect(response.body.size).to eq 10
     end
   end
+  describe "GET /api/v1/kids/:id/communication_notebooks/edit" do
+    it "ある子供に紐づく連絡帳を全て取得するAPI" do
+      get "/api/v1/kids/#{kid.id}/communication_notebooks/"
+      target_date = "2021-04-25xxxxxxxxx"
+      new_date = target_date.slice(0..9)
+      communication_notebook = CommunicationNotebook.where(date: target_date)
+      FactoryBot.create_list(:communication_notebook, 10, kid_id:kid.id)
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(response.body.size).to eq 10
+    end
+  end
 end
