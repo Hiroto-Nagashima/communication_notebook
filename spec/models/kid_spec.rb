036478b66fac6d5ebfffd6db94efdb80e1f575cc
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-25 13:49:36 +0900

    [add]Kidのバリーデーションテスト作成

--- spec/models/kid_spec.rb
+++ spec/models/kid_spec.rb
@@ -16,4 +16,37 @@ RSpec.describe Kid, type: :model do
       end
     end
   end
+
+  describe '入力値の有無' do
+    it '名前が空欄の場合保存に失敗するか' do
+      kid = Kid.new
+      expect(kid). not_to be_valid
+      expect(kid.errors.messages[:name]).to include("can't be blank")
+    end
+
+    it 'メールアドレスが空欄の場合保存に失敗するか' do
+      kid = Kid.new
+      expect(kid). not_to be_valid
+      expect(kid.errors.messages[:email]).to include("can't be blank")
+    end
+
+    it '年齢が空欄の場合保存に失敗するか' do
+      kid = Kid.new
+      expect(kid). not_to be_valid
+      expect(kid.errors.messages[:age]).to include("can't be blank")
+    end
+
+    it '保育園の名前が空欄の場合保存に失敗するか' do
+      kid = Kid.new
+      expect(kid). not_to be_valid
+      expect(kid.errors.messages[:daycare_name]).to include("can't be blank")
+    end
+
+    it 'パスワードが空欄の場合保存に失敗するか' do
+      kid = Kid.new
+      expect(kid). not_to be_valid
+      expect(kid.errors.messages[:password]).to include("can't be blank")
+    end
+
+  end
 end
