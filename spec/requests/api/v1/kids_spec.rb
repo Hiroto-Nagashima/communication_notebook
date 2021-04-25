ad86cf00d460030da67beebdbfb72f3993c38ac3
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-25 18:59:35 +0900

    [update]IDをもとに特定のkidを取得するAPIテスト完了

--- spec/requests/api/v1/kids_spec.rb
+++ spec/requests/api/v1/kids_spec.rb
@@ -3,16 +3,11 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Kids", type: :request do
   describe "GET /api/v1/kids" do
     it '入力したidを持った子供のデータ取得' do
-      kid = create(:kid, name: '高田弘樹')
-
+      kid = create(:kid)
       get "/api/v1/kids/#{kid.id}"
       json = JSON.parse(response.body)
-
-      # リクエスト成功を表す200が返ってきたか確認する。
       expect(response.status).to eq(200)
-
-      # 要求した特定のポストのみ取得した事を確認する
-      expect(json['data']['name']).to eq(kid.name)
+      expect(json['name']).to eq(kid.name)
     end
   end
 end
