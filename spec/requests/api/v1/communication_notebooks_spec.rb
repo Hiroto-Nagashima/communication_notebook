52d4849cd5490bdb5a5c43aa8e48d32c5d8432f6
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-26 00:58:13 +0900

    [update]editアクションのテスト完了

--- spec/requests/api/v1/communication_notebooks_spec.rb
+++ spec/requests/api/v1/communication_notebooks_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe "Api::V1::CommunicationNotebooks", type: :request do
   end
   describe "GET /api/v1/kids/:id/communication_notebooks/edit" do
     it "ある子供に紐づく連絡帳を全て取得するAPI" do
-      FactoryBot.create_list(:communication_notebook, 3, kid_id:kid.id, date: "2021-4-26")
-      get "/api/v1/kids/#{kid.id}/communication_notebooks/edit"
-      target_date = "2021-04-26xxxxxxxxx"
+      FactoryBot.create_list(:communication_notebook, 10, kid_id:kid.id, date: "2021-4-26")
+      FactoryBot.create_list(:communication_notebook, 3, kid_id:kid.id, date: "2021-4-27")
+      get "/api/v1/kids/#{kid.id}/communication_notebooks/edit", params:{ target_date: "2021-04-26xxxxxxxxx"}
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(json.size).to eq 3
+      expect(json.size).to eq 10
     end
   end
 end
