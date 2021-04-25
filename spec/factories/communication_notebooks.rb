2689d1dcc4e339687fb4fd577af5e7997dd91caf
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-26 00:39:35 +0900

    [fix]ある子供に紐づく連絡帳を全て取得するAPIのテスト完了

--- spec/factories/communication_notebooks.rb
+++ spec/factories/communication_notebooks.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     memo             { "メモ" }
     bodyTemperature  { "体温" }
     mood             { 0 }
-    date             "2021-4-25"
+    date             {"2021-4-25"}
     bath             { "有" }
     kid_id           { 1 }
   end
--- spec/requests/api/v1/communication_notebooks_spec.rb
+++ spec/requests/api/v1/communication_notebooks_spec.rb
@@ -3,24 +3,22 @@ require 'rails_helper'
 RSpec.describe "Api::V1::CommunicationNotebooks", type: :request do
   let(:kid){create :kid}
   describe "GET /api/v1/kids/:id/communication_notebooks" do
-    xit "ある子供に紐づく連絡帳を全て取得するAPI" do
+    it "ある子供に紐づく連絡帳を全て取得するAPI" do
       FactoryBot.create_list(:communication_notebook, 10, kid_id:kid.id)
       get "/api/v1/kids/#{kid.id}/communication_notebooks"
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(response.body.size).to eq 10
+      expect(json.size).to eq 10
     end
   end
   describe "GET /api/v1/kids/:id/communication_notebooks/edit" do
     it "ある子供に紐づく連絡帳を全て取得するAPI" do
-      get "/api/v1/kids/#{kid.id}/communication_notebooks/"
-      target_date = "2021-04-25xxxxxxxxx"
-      new_date = target_date.slice(0..9)
-      communication_notebook = CommunicationNotebook.where(date: target_date)
-      FactoryBot.create_list(:communication_notebook, 10, kid_id:kid.id)
+      FactoryBot.create_list(:communication_notebook, 3, kid_id:kid.id, date: "2021-4-26")
+      get "/api/v1/kids/#{kid.id}/communication_notebooks/edit"
+      target_date = "2021-04-26xxxxxxxxx"
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(response.body.size).to eq 10
+      expect(json.size).to eq 3
     end
   end
 end
