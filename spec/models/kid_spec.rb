ab02ef8f77ddb8aeb9a8f7f7c98e5e8624671e90
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-25 12:18:16 +0900

    [add] kidのデータが保存できるかのテスト追加

--- spec/models/kid_spec.rb
+++ spec/models/kid_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Kid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '正常系の機能' do
+    context '保存する' do
+      it '名前、年齢、保育園名、メールアドレスがあれば成功' do
+        kid = Kid.new(
+          name: "前田たけし",
+          age: 1,
+          daycare_name: "埼玉保育園",
+          email: "12345@gmail.com",
+          password: 1234567
+        )
+        expect(kid).to be_valid
+        kid.save
+      end
+    end
+  end
 end
