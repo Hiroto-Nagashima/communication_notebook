4949e06f93149fee5b4315fd111aeda3c5da21eb
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-26 23:12:12 +0900

    [fix]Registrationページ整理

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -8,40 +8,49 @@ import { UserContext } from "../provider/UserProvider";
 import format from "date-fns/format";
 import queryString from 'query-string';
 
-export const RegistrationPage:VFC= memo(()=>{
+export const RegistrationPage:VFC= ()=>{
+  const { kidId } = useContext(UserContext)
+  const { state } = useLocation<Date>()
+  const location = useLocation()
+  const history = useHistory()
+  const newDate = format(state!, 'yyyy/MM/dd')
+  const qs = queryString.parse(location.search)
+
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
+  const [open, setOpen] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false)
   const [dinner, setDinner] = useState<string | null>(null);
+  const [breakfast, setBreakfast] = useState<string | null>(null);
+  const [memo, setMemo] = useState<string | null>(null);
+  const [bodyTemperature, setBodyTemperature] = useState<number |string| null>(null);
+  const [bath, setBath] = useState<string | null>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(1);
+
   const handleDinnerChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
     setDinner(e.target.value);
     console.log("1")
   },[])
 
-  const [breakfast, setBreakfast] = useState<string | null>(null);
   const handleBreakfastChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
     setBreakfast(e.target.value);
     console.log("2")
   },[])
 
-  const [memo, setMemo] = useState<string | null>(null);
   const handleMemoChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
     setMemo(e.target.value);
   }, [])
 
-  const [bodyTemperature, setBodyTemperature] = useState<number |string| null>(null);
   const handleBodyTemperatureChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
     setBodyTemperature(e.target.value);
   },[])
-
-  const [ bath, setBath ] = useState<string | null>(null);
 
   const handleBathChange = useCallback((e:ChangeEvent<HTMLInputElement>) => {
     setBath((e.target as HTMLInputElement).value);
     console.log(bath)
   },[]);
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const handleClose = useCallback(() => {
     setAnchorEl(null);
   },[])
@@ -49,14 +58,12 @@ export const RegistrationPage:VFC= memo(()=>{
   const handleClick = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   },[])
-  const [selectedIndex, setSelectedIndex] = useState<number>(1);
+
   const handleMenuItemClick = useCallback((event: React.MouseEvent<HTMLElement>, index: number) => {
     setSelectedIndex(index);
     setAnchorEl(null);
     console.log(selectedIndex)
   },[])
-
-  const [open, setOpen] = useState(false);
 
   const handleDialogOpen = useCallback(() => {
     setOpen(true);
@@ -65,8 +72,7 @@ export const RegistrationPage:VFC= memo(()=>{
   const handleDialogClose = useCallback(() => {
     setOpen(false);
   },[]);
-  const { kidId } = useContext(UserContext)
-  const history = useHistory()
+
   const handleClickRegister=() =>{
     if(isUpdate){
       axios
@@ -106,21 +112,13 @@ export const RegistrationPage:VFC= memo(()=>{
       .catch((e)=>console.log(e))
   }
 
-//以下のstateはTop.tsxから遷移してきた時に送られてくる。DatePickerで選択した日付が入っている.
-  const { state } = useLocation<Date>()
-  const location = useLocation()
-  const newDate = format(state!, 'yyyy/MM/dd')
-  const qs = queryString.parse(location.search)
-  console.log(qs.id)
-
   const fetchNewNotebook =()=>(
     axios
     .get<CommunicationNotebook>(`http://localhost:3000/api/v1/kids/${ kidId }/communication_notebooks/registration`,{
       params:{
         target_date: state
       }
-      }
-    )
+    })
     .then((res)=>{
       console.log(res.data)
       setDinner(res.data.dinner)
@@ -134,7 +132,6 @@ export const RegistrationPage:VFC= memo(()=>{
     )
     .catch((e)=> {
       setError(e)
-      console.log(e)
     })
     .finally(() => setLoading(false))
   )
@@ -144,8 +141,7 @@ export const RegistrationPage:VFC= memo(()=>{
       params:{
         target_date: state
       }
-      }
-    )
+    })
     .then((res)=>{
       console.log(res.data)
       setDinner(res.data[0].dinner)
@@ -160,11 +156,9 @@ export const RegistrationPage:VFC= memo(()=>{
     )
     .catch((e)=> {
       setError(e)
-      console.log(e)
     })
     .finally(() => setLoading(false))
   )
-
 
   useEffect(()=>{
     if(location.search){
@@ -176,37 +170,37 @@ export const RegistrationPage:VFC= memo(()=>{
 
   return (
     <>
-    {loading?
-      <CircularDeterminate/>
-      :error?(
-        <h1>エラー</h1>
-      ):(
-        <>
-          <InputOfNotebook
-            selectedDate={newDate}
-            dinner={dinner}
-            onChangeDinner={handleDinnerChange}
-            breakfast={breakfast}
-            onChangeBreakfast={handleBreakfastChange}
-            memo={memo}
-            onChangeMemo={handleMemoChange}
-            bath={bath}
-            onChangeBath={handleBathChange}
-            body_temperature={bodyTemperature}
-            onChangeBodyTemperature={handleBodyTemperatureChange}
-            anchorEl={anchorEl}
-            index={selectedIndex}
-            onClose={handleClose}
-            onClickMenu={handleMenuItemClick}
-            onClickButton={handleClick}
-            isOpen={open}
-            onClickDialogClose={handleDialogClose}
-            onClickDialogOpen={handleDialogOpen}
-            onClickRegister={handleClickRegister}
+      {loading?
+        <CircularDeterminate/>
+        :error?(
+          <h1>エラー</h1>
+        ):(
+          <>
+            <InputOfNotebook
+              selectedDate={newDate}
+              dinner={dinner}
+              onChangeDinner={handleDinnerChange}
+              breakfast={breakfast}
+              onChangeBreakfast={handleBreakfastChange}
+              memo={memo}
+              onChangeMemo={handleMemoChange}
+              bath={bath}
+              onChangeBath={handleBathChange}
+              body_temperature={bodyTemperature}
+              onChangeBodyTemperature={handleBodyTemperatureChange}
+              anchorEl={anchorEl}
+              index={selectedIndex}
+              onClose={handleClose}
+              onClickMenu={handleMenuItemClick}
+              onClickButton={handleClick}
+              isOpen={open}
+              onClickDialogClose={handleDialogClose}
+              onClickDialogOpen={handleDialogOpen}
+              onClickRegister={handleClickRegister}
             />
           </>
-      )
-    }
+        )
+      }
     </>
   )
-})
+}
