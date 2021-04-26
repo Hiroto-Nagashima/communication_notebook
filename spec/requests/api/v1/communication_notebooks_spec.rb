f8adfef1d13798b47e9bbd44378ee91be6e44015
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-26 11:06:32 +0900

    [update]updateアクションのテスト完了

--- spec/requests/api/v1/communication_notebooks_spec.rb
+++ spec/requests/api/v1/communication_notebooks_spec.rb
@@ -47,10 +47,11 @@ RSpec.describe "Api::V1::CommunicationNotebooks", type: :request do
   end
   describe "Put /api/v1/kids/:id/communication_notebooks/:id" do
     it "ある子供に紐づく連絡帳を更新するAPI" do
-      # valid_params= attributes_for(:communication_notebook)
-      # expect { post "/api/v1/kids/#{kid.id}/communication_notebooks", params:{communication_notebook: valid_params }}
-      # .to change(CommunicationNotebook, :count).by(+1)
-      # expect(response).to have_http_status(200)
+      target_notebook = create(:communication_notebook, kid_id: kid.id, memo: "普通です", date: "2021-4-26")
+      put "/api/v1/kids/#{kid.id}/communication_notebooks/#{target_notebook.id}", params: {communication_notebook: {memo: "元気です"},target_date: "2021-4-26"}
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(target_notebook.reload.memo).to eq("元気です")
     end
   end
 end
