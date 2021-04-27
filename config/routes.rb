abc99e002dcc94d88dbeeba515fcc5757c915e4b
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-04-27 14:12:51 +0900

    [update]resourcesの範囲を限定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :kids do
+      resources :kids, only: [:show] do
         get '/communication_notebooks/doesExist', to: 'communication_notebooks#doesExist'
         get '/communication_notebooks/registration', to: 'communication_notebooks#registration'
-        resources :communication_notebooks
+        resources :communication_notebooks, only: [:index, :create, :update]
       end
     end
   end
